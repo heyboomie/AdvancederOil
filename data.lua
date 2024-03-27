d28d4904f28739fa2e01fb2b17c493a013a46ca7
heyboomie <109445551+heyboomie@users.noreply.github.com>
2024-03-27 11:38:19 -0400

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,1 +1,14 @@
-local contaminatedOil = table.deepcopy(data.raw["fluid"]["crude-oil"]
+{  
+  type = "fluid",
+  name = "contaminated-oil",
+  order = "b[fluid]-b[c
+  icon = "__advanceder-oil__/graphics/contaminatedOil.png",
+  icon_size = 64,
+  default_temperatire = 25,
+  base_color = {r=112, g=108, b=98, a=255},
+  flow_color = {r=112, g=108, b=98, a=255},
+  heat_capacity = "4.5KJ",
+  emissions_multiplier = "3",
+  gas_temperature = "50",
+  auto_barrel = "True",
+}    
