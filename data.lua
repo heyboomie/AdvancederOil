cbd3460bc5ad0ecf49d58e78bd178e571bffdf05
heyboomie <109445551+heyboomie@users.noreply.github.com>
2024-03-28 09:06:59 -0400

    Update data.lua

--- data.lua
+++ data.lua
@@ -7,7 +7,6 @@
   base_color = {r=112, g=108, b=98, a=255},
   flow_color = {r=112, g=108, b=98, a=255},
   heat_capacity = "4.5KJ",
-  emissions_multiplier = "3",
   gas_temperature = "50",
   auto_barrel = "True",
 }    
