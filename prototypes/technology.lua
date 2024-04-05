6bcc7374d531716f4959846205ec198bd95756a7
heyboomie <109445551+heyboomie@users.noreply.github.com>
2024-04-05 09:07:02 -0400

    Create technology.lua

---
+++ prototypes/technology.lua
@@ -0,0 +1,21 @@
+local contaminated-oil-processing-research = {
+  type = "technology",
+  name = "contaminated-oil-processing-research",
+  icon_size = 64,
+  icon = "__advanceder-oil__/graphics/contaminatedoilProcessing.png",
+  effects = 
+  {
+    {
+      type = "unlock_recipe",
+      recipe = "contaminated-oil-processing"
+    },
+    {
+      type = "unlock_recipe",
+      recipe = "shall-oil-extraction"
+    },  
+    {
+      type = "unlock_recipe",
+      recipe = "shall-fracker"
+    },
+  }
+  
