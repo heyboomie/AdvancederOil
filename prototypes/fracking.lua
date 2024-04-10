ad72cc83e61b2ef75cafe230de48bf0ca5ea7e5a
heyboomie <109445551+heyboomie@users.noreply.github.com>
2024-04-10 12:57:01 -0400

    Update fracking.lua

--- prototypes/fracking.lua
+++ prototypes/fracking.lua
@@ -1,2 +1,12 @@
-local shall-fracker = {
-  type
+data:extend(
+{
+  {
+    type = "item",
+    name = "frac-pump",
+    icon = "__base__/graphics/icons/frac-pump.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "extraction-machine",
+    order = "b[fluids]-b[frac-pump]",
+    place_result = "frac-pump",
+    stack_size = 20
+  },
